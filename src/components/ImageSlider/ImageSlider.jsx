4d66d509800ceb42b732cf5d1924d5e29f87a599
JZWe <ArturiaTW@gmail.com>
2021-08-14 11:47:11 +0800

    Edit ImageSlider
    Originallly use <img/> code
    Edit it to use <Box/> and background-image

--- src/components/ImageSlider/ImageSlider.jsx
+++ src/components/ImageSlider/ImageSlider.jsx
@@ -1,18 +1,21 @@
 import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
-import { Container, Box } from '@material-ui/core';
+import { Box, Typography, Button } from '@material-ui/core';
+
 import useStyles from './styles';
 
 const ImageSlider = ({ slides }) => {
+  const history = useHistory();
   const [current, setCurrent] = useState(0);
-  const length = slides.length;
+  const { length } = slides;
   const classes = useStyles();
 
   useEffect(() => {
     const intervalID = setInterval(() => {
       nextSlide();
-    }, 4000);
+    }, 5000);
 
     return () => {
       clearInterval(intervalID);
@@ -27,44 +30,68 @@ const ImageSlider = ({ slides }) => {
   const nextSlide = () =>
     setCurrent((prev) => (prev === length - 1 ? 0 : prev + 1));
 
+  const moveToProductsPage = () => {
+    history.push('/products');
+  };
+
+  const renderSlides = () => {
+    return slides.map((slide, index) => {
+      return (
+        <Box
+          className={
+            current === index
+              ? `${classes.slide} ${classes['slide-active']}`
+              : classes['slide']
+          }
+          key={index}
+          style={
+            current === index
+              ? {
+                  backgroundImage: `url(${slide})`,
+                }
+              : null
+          }
+        >
+          <Box className={classes.colorOverlay} />
+        </Box>
+      );
+    });
+  };
+
+  const renderDots = () => {
+    const dots = Array.from({ length }).map((item, index) => (
+      <Box
+        onClick={() => setCurrent(index)}
+        key={index}
+        className={
+          current === index
+            ? `${classes.dot} ${classes['dot-active']}`
+            : classes.dot
+        }
+      ></Box>
+    ));
+    return dots;
+  };
+
   return (
-    <Container className={classes.slider}>
+    <Box className={classes.slider}>
       <ArrowBackIcon className={classes['left-arrow']} onClick={prevSlide} />
       <ArrowForwardIcon
         className={classes['right-arrow']}
         onClick={nextSlide}
       />
+      {renderSlides()}
+      <Typography className={classes.title}>
+        Welcome to Fake Plushies Shop!
+      </Typography>
 
-      {slides.map((slide, index) => {
-        return (
-          <Box
-            className={
-              current === index
-                ? `${classes.slide} ${classes['slide-active']}`
-                : classes['slide']
-            }
-            key={index}
-          >
-            {index === current && (
-              <img src={slide} className={classes.img} alt="azurlane img" />
-            )}
-          </Box>
-        );
-      })}
-      <Box className={classes['container-dots']}>
-        {Array.from({ length }).map((item, index) => (
-          <Box
-            onClick={() => setCurrent(index)}
-            key={index}
-            className={
-              current === index
-                ? `${classes.dot} ${classes['dot-active']}`
-                : classes.dot
-            }
-          ></Box>
-        ))}
-      </Box>
-    </Container>
+      <p className={classes.paragraph}>
+        <Button className={classes.whiteButton} onClick={moveToProductsPage}>
+          CHECK PRODUCTS
+        </Button>
+      </p>
+      <Box className={classes['container-dots']}>{renderDots()}</Box>
+    </Box>
   );
 };
 
